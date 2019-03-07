5790efe2c543bfea7dcd549e4a722895851d811b
Kurs <Kurs@172.16.99.144>
2019-03-07 15:33:25 +0100

    quadrat gezeichnet

--- src/com/hoffrogge/lehreinheit01/TetrominoQ.java
+++ src/com/hoffrogge/lehreinheit01/TetrominoQ.java
@@ -1,7 +1,25 @@
 package com.hoffrogge.lehreinheit01;
 
+import java.awt.Graphics;
+
 import com.hoffrogge.lehreinheit04.Quadrat;
 
 public class TetrominoQ extends Quadrat {
+
+	public void zeichnen(Graphics graphics) {
+		super.zeichnen(graphics);
+
+		Rechteck erstesRechteck = new Rechteck(50, 50);
+		erstesRechteck.zeichnen(graphics);
+
+		Rechteck zweitesRechteck = new Rechteck(100, 50);
+		zweitesRechteck.zeichnen(graphics);
+
+		Rechteck drittesRechteck = new Rechteck(100, 100);
+		drittesRechteck.zeichnen(graphics);
+
+		Rechteck viertesRechteck = new Rechteck(150, 50);
+		viertesRechteck.zeichnen(graphics);
+	}
 
 }
