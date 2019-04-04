01cc5d17cd742496d47a56c2c1bfda1f3e9d484e
Kurs <Kurs@172.16.99.144>
2019-04-04 15:30:52 +0200

    keylistener

--- src/com/hoffrogge/lehreinheit07/DemoKeyListener.java
+++ src/com/hoffrogge/lehreinheit07/DemoKeyListener.java
@@ -2,6 +2,7 @@ package com.hoffrogge.lehreinheit07;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 import javax.swing.JLabel;
 
@@ -12,6 +13,11 @@ public class DemoKeyListener implements KeyListener {
 	public DemoKeyListener(JLabel textLabel) {
 		this.textLabel = textLabel;
 	}
+
+	private int Ergebnis = 0;
+	private int ziffer = 0;
+	private ArrayList<Integer> ziffernListe = new ArrayList<Integer>();
+	private int zwischenErgebnis = 0;
 
 	@Override
 	public void keyPressed(KeyEvent e) {
@@ -29,11 +35,24 @@ public class DemoKeyListener implements KeyListener {
 	}
 
 	private void gebeTasteAus(KeyEvent e, String eventArt) {
-		textLabel.setText("Taste " + e.getKeyChar() + " " + eventArt);
+		// textLabel.setText("Taste " + e.getKeyChar() + " " + eventArt);
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// nichts machen
+		if (e.getKeyChar() >= 48 && e.getKeyChar() <= 58) {
+			ziffer = e.getKeyChar() - 48;
+			ziffernListe.add(ziffer);
+			Ergebnis = Ergebnis + ziffer;
+			textLabel.setText("Ergebnis =" + Ergebnis);
+			if (KeyEvent.VK_ENTER == e.getKeyCode()) {
+				for (int i = 0; i < ziffernListe.size(); i++) {
+					System.out.println(ziffernListe.get(i));
+				}
+
+			}
+
+		}
+
 	}
 }
